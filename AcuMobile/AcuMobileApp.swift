59c2453a399d597b8a68f20676764779ca57163a
virajkulkarni1 <kviraj223@gmail.com>
2026-02-27 19:13:46 -0500

    Initial Commit

---
+++ AcuMobile/AcuMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcuMobileApp.swift
+//  AcuMobile
+//
+//  Created by Viraj Kulkarni on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct AcuMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
